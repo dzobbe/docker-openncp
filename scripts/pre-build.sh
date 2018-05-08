9d81f2e34773a56102687f69771024fb0cd4691d
Giovanni Mazzeo <mazzeo.gio@gmail.com>
2018-05-08 15:22:13 +0200

    New content

--- scripts/pre-build.sh
+++ scripts/pre-build.sh
@@ -6,6 +6,8 @@ if [ ! -d wars ]; then
   mkdir -p wars;
 fi
 
+pushd scripts
+
 wget https://ec.europa.eu/cefdigital/artifact/content/repositories/eHealth/eu/europa/ec/sante/ehdsi/openncp-protocol-terminators/openncp-ncp-server/openncp-ws-server/${OPENNCP_WARS_VERSION}/openncp-ws-server-${OPENNCP_WARS_VERSION}.war -O ../wars/openncp-ws-server.war
 wget https://ec.europa.eu/cefdigital/artifact/content/repositories/eHealth/eu/europa/ec/sante/ehdsi/openncp-protocol-terminators/openncp-ncp-client/openncp-client-connector/${OPENNCP_WARS_VERSION}/openncp-client-connector-${OPENNCP_WARS_VERSION}.war -O ../wars/openncp-client-connector.war
 wget https://ec.europa.eu/cefdigital/artifact/content/repositories/eHealth/eu/europa/ec/sante/ehdsi/openncp-protocol-terminators/openncp-ncp-client/openncp-client-connector/${OPENNCP_WARS_VERSION}/openncp-client-connector-${OPENNCP_WARS_VERSION}.war -O ../wars/openncp-client-connector.war
@@ -13,3 +15,5 @@ wget https://ec.europa.eu/cefdigital/artifact/content/repositories/eHealth/eu/eu
 wget https://ec.europa.eu/cefdigital/artifact/content/repositories/eHealth/eu/europa/ec/sante/ehdsi/openncp-trc-sts/${OPENNCP_WARS_VERSION}/openncp-trc-sts-${OPENNCP_WARS_VERSION}.war -O ../wars/TRC-STS.war
 wget https://ec.europa.eu/cefdigital/artifact/content/repositories/eHealth/eu/europa/ec/sante/ehdsi/openncp-gateway/${OPENNCP_WARS_VERSION}/openncp-gateway-${OPENNCP_WARS_VERSION}.war -O ../wars/openncp-gateway.war
 wget https://ec.europa.eu/cefdigital/artifact/content/repositories/eHealth/eu/europa/ec/sante/ehdsi/openncp-portal/${OPENNCP_WARS_VERSION}/openncp-portal-${OPENNCP_WARS_VERSION}.war -O ../wars/epsosportal.war
+
+popd
