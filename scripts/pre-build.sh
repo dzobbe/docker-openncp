8a2a658f3056b7939300a540811ff060d7ace23b
Giovanni <giovanni.mazzeo@uniparthenope.it>
2018-07-23 14:09:11 +0200

    Added wars

--- scripts/pre-build.sh
+++ scripts/pre-build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+USER=
+PWD=
 OPENNCP_WARS_VERSION=2.5.2
 
 
@@ -9,7 +12,7 @@ fi
 
 pushd scripts
 
-wget https://ec.europa.eu/cefdigital/artifact/content/repositories/eHealth/eu/europa/ec/sante/ehdsi/openncp-protocol-terminators/openncp-ncp-server/openncp-ws-server/${OPENNCP_WARS_VERSION}/openncp-ws-server-${OPENNCP_WARS_VERSION}.war -O ../wars/openncp-ws-server.war
+wget --user ${USER} --password ${PWD} https://ec.europa.eu/cefdigital/artifact/content/repositories/eHealth/eu/europa/ec/sante/ehdsi/openncp-protocol-terminators/openncp-ncp-server/openncp-ws-server/${OPENNCP_WARS_VERSION}/openncp-ws-server-${OPENNCP_WARS_VERSION}.war -O ../wars/openncp-ws-server.war
 wget https://ec.europa.eu/cefdigital/artifact/content/repositories/eHealth/eu/europa/ec/sante/ehdsi/openncp-protocol-terminators/openncp-ncp-client/openncp-client-connector/${OPENNCP_WARS_VERSION}/openncp-client-connector-${OPENNCP_WARS_VERSION}.war -O ../wars/openncp-client-connector.war
 wget https://ec.europa.eu/cefdigital/artifact/content/repositories/eHealth/eu/europa/ec/sante/ehdsi/openncp-openatna/openatna-web/${OPENNCP_WARS_VERSION}/openatna-web-${OPENNCP_WARS_VERSION}.war -O ../wars/openatna-web.war
 wget https://ec.europa.eu/cefdigital/artifact/content/repositories/eHealth/eu/europa/ec/sante/ehdsi/openncp-trc-sts/${OPENNCP_WARS_VERSION}/openncp-trc-sts-${OPENNCP_WARS_VERSION}.war -O ../wars/TRC-STS.war
